9a39e62897d58a11a729a5ff39901b1b9382445e
Tamce <tamce@outlook.com>
2017-03-13 00:32:34 +0800

    基础功能都完成了
    现在根据接口的情况为每个号码单独调用接口来发送短信。

--- main.py
+++ main.py
@@ -11,15 +11,17 @@ from hashlib import sha1
 from urllib.parse import urlencode
 from httplib2 import Http
 
-# 毫无用处的全局变量声明
-global queue
+# 毫无用处的全局变量声明，只是为了标识哪些变量在局部作用域使用了
+global rowParams
 global mobiles
 global http
 global logFile
+global result
 
-queue = []
+rowParams = []
 mobiles = []
 http = Http()
+result = []
 
 # 切换目录到脚本运行目录，并读入配置
 os.chdir(os.path.split(os.path.realpath(__file__))[0])
@@ -29,8 +31,13 @@ f.close()
 logFile = open(config['log-file'], mode='a', encoding='utf-8')
 
 
-def log(content = None, msgType = 'Message', pure = None) :
+# 函数无论如何均返回 True
+def log(content = None, msgType = 'Message', pure = None, level = 1) :
     global logFile
+    # 仅当事件 level 小于或等于 config['log']['level'] 的记录时才进行记录
+    if level > config['log']['level'] :
+        return True
+
     if pure is None :
         logFile.write(time.asctime() + '\t' + msgType + ':\t' + content)
     else :
@@ -52,21 +59,21 @@ def printInfo() :
 
 # 读 Excel 文件
 def read(f = None) :
-    global queue
+    global rowParams
     global mobiles
     if f is None :
         f = input('请输入要处理的 Excel 文档 > ')
-    log('Reading file: ' + f + '...')
+    log('Reading file: ' + f + '...', level = 1)
     # 读取表格信息
     book = xlrd.open_workbook(f)
     table = book.sheet_by_index(0)
-    log('File opened.')
+    log('File opened.', level = 2)
     for i in range(table.nrows) :
         # 第一列留作电话号码列，从第二列开始作为变量列读入
         mobiles.append(table.row_values(i)[0])
-        queue += table.row_values(i)[1 : 1 + config['var-count']]
-    # 读取完毕，所有信息读入到 queue 中
-    log('Table analysis complete.')
+        rowParams.append(table.row_values(i)[1 : 1 + config['var-count']])
+    # 读取完毕，所有信息读入到 rowParams 中
+    log('Table analysis complete.', level = 2)
     book.release_resources()
 
 
@@ -76,12 +83,12 @@ def check() :
     print(config['template']['content'])
     print('------------------------------')
     for i in range(len(mobiles)) :
-        print(str(mobiles[i]) + '\t' + '\t'.join(queue[i * config['var-count']:(i + 1) * config['var-count']])) 
+        print(str(mobiles[i]) + '\t' + '\t'.join(rowParams[i])) 
 
 
 # 获取调用接口所需要的请求头
 def getHeader() :
-    log('Getting HTTP Headers...')
+    log('Getting HTTP Headers...', level = 5)
     random.seed()
     curTime = str(int(time.time()))
     nonce = ''.join(random.choices('0123456789', k = 32))
@@ -95,31 +102,40 @@ def getHeader() :
     }
 
 # 发送模板信息的接口调用
-def send() :
-    log('Ready to send request to https://api.netease.im/sms/sendtemplate.action...')
+# mobile -> string 电话号码
+# params -> list 参数
+def sendSingle(mobile, params) :
+    log('Sending message to: %s with params %s' % (mobile, ','.join(params)), level = 3)
     header = getHeader()
     body = urlencode({
         'templateid': config['template']['id'],
-        'mobiles': json.dumps(mobiles),
-        'params': json.dumps(queue)
+        'mobiles': json.dumps([mobile]),
+        'params': json.dumps(params)
     })
-    log(pure = '\n' + str(header) + '\n' + body + '\n--------')
+    log(pure = str(header) + '\n' + body + '\n--------', level = 4)
     # https://api.netease.im/sms/sendtemplate.action
     resp, body = http.request('https://api.netease.im/sms/sendtemplate.action',
         method='POST',
         headers = header,
         body = body
     )
-    log('Request complete, response:')
-    print(resp)
-    print(body)
-    log(pure = '\n' + str(resp) + '\n' + str(body) + '\n--------')
+    result.append([{"mobile": mobile, "params": params}, json.loads(body)])
+    log('Request complete, response:', level = 4)
+    log(pure = str(resp) + '\n' + str(body) + '\n--------', level = 4)
+    log(pure = body, level = 3)
+    print('%s - %s \t-> ' % (mobile, ','.join(params), body.decode()))
+
+
+def send() :
+    log('Start sending message for each one...', level = 1)
+    for i in range(len(mobiles)) :
+        sendSingle(mobiles[i], rowParams[i])
 
 
 # 清空读入的信息
 def clear() :
-    log('Clearing readed data..')
-    queue.clear()
+    log('Clearing readed data..', level = 2)
+    rowParams.clear()
     mobiles.clear()
 
 
@@ -134,6 +150,7 @@ def action() :
     'q > 退出程序\n'
     '-----------'
     )
+
     choice = input('请选择 > ')
     try:
         {
@@ -141,13 +158,13 @@ def action() :
             'b': check,
             'c': send,
             'd': clear,
-            'q': lambda : log('Program exit.') and exit(),
+            'q': lambda : log('Program exit.', level = 1) and exit(),
         }[choice]()
     except KeyError:
         print('\n输入了错误的选择！\n')
 
 
-log('Program bootstrap complete... Running...')
+log('Program bootstrap complete... Running...', level = 1)
 try:
     read(config['excel-file'])
     while True:
@@ -157,6 +174,6 @@ except BaseException:
     if (type_ is SystemExit) :
         exit()
     err = ''.join(traceback.format_exception(type_, value_, traceback_))
-    log(pure = time.asctime() + '\tError:\tAn Exception has been thrown when processing...\n' + err + '\n---------')
+    log(pure = time.asctime() + '\tError:\tAn Exception has been thrown when processing...\n' + err + '\n---------', level = 1)
     print(err)
 
